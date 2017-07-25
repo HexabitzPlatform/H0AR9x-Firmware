8d74fa5cc4632d6065c75f2fccdf32c63a165ebc
DESKTOP-V96NR7T\Asaad <asaad@hexabitz.com>
2017-07-24 23:26:30 -0700

    Completed HDC development Not working yet

--- H12R0/H12R0.c
+++ H12R0/H12R0.c
@@ -29,6 +29,7 @@ UART_HandleTypeDef huart4;
 UART_HandleTypeDef huart5;
 UART_HandleTypeDef huart6;
 
+HDC_t HDC;
 
 /* Private variables ---------------------------------------------------------*/
 
@@ -128,10 +129,32 @@ uint8_t GetPort(UART_HandleTypeDef *huart)
 */
 Module_Status HDC_Init(void)
 {
-	Module_Status result = H12R0_OK;
-
+	uint16_t manID = 0, DevID = 0, serialID[3] = {0};
+	uint8_t reg = 0;
 	
-	return result;
+	/* Read IDs */
+	reg = HDC_MANUFACTURER_ID;
+	HAL_I2C_Master_Transmit(&hi2c2, HDC_ADD, &reg, HDC_REG_ADD_SIZE, 10);
+	HAL_I2C_Master_Receive(&hi2c2, HDC_ADD, (uint8_t *)&manID, HDC_REG_SIZE, 10);
+	//HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_MANUFACTURER_ID, HDC_REG_ADD_SIZE, (uint8_t *)&manID, HDC_REG_SIZE, 10);
+	HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_DEVICE_ID, HDC_REG_ADD_SIZE, (uint8_t *)&DevID, HDC_REG_SIZE, 10);
+	HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_SERIAL_ID_1, HDC_REG_ADD_SIZE, (uint8_t *)&serialID[0], HDC_REG_SIZE, 10);
+	HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_SERIAL_ID_2, HDC_REG_ADD_SIZE, (uint8_t *)&serialID[1], HDC_REG_SIZE, 10);
+	HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_SERIAL_ID_3, HDC_REG_ADD_SIZE, (uint8_t *)&serialID[2], HDC_REG_SIZE, 10);
+	
+	/* Check Manufacturer ID */
+	if (manID != 0x5449)	
+		return H12R0_ERR_HDC1080_MID;
+	
+	/* Configure for sequence acquisition, 14-bit temperature and 14-bit humidity */
+	if (HDC_Config() != H12R0_OK)
+		return H12R0_ERR_HDC1080_CONFIG;
+	
+	/* Read the sensors */
+	if (HDC_Read(HDC_BOTH) != H12R0_OK)
+		return H12R0_ERR_HDC1080_READ;
+	
+	return H12R0_OK;
 }
 
 /*-----------------------------------------------------------*/
@@ -173,12 +196,113 @@ Module_Status APDS_Init(void)
 /*-----------------------------------------------------------*/
 
 
+
 /* -----------------------------------------------------------------------
 	|																APIs	 																 	|
    ----------------------------------------------------------------------- 
 */
 
+/* --- Configure HDC1080DMBT. 
+*/
+Module_Status HDC_Config(void)
+{
+	uint16_t reg = 0;
+	
+	/* Parse the configuration 
+	*/
+	if (HDC.config.reset == HDC_RESET)
+		HDC_CONFIG_RESET(reg);
+	
+	if (HDC.config.heat == HDC_HEAT_ENABLE)
+		HDC_CONFIG_HEAT_ENABLE(reg);		
+	else if (HDC.config.heat == HDC_HEAT_DISABLE)
+		HDC_CONFIG_HEAT_DISABLE(reg);	
+	
+	if (HDC.config.mode == HDC_MODE_TEMP_AND_HUMID)
+		HDC_CONFIG_TEMP_AND_HUMID(reg);		
+	else if (HDC.config.mode == HDC_MODE_TEMP_OR_HUMID)
+		HDC_CONFIG_TEMP_OR_HUMID(reg);	
+	
+	if (HDC.config.temp_res == HDC_RES_14_BITS)
+		HDC_CONFIG_TEMP_14_BITS(reg);		
+	else if (HDC.config.temp_res == HDC_RES_11_BITS)
+		HDC_CONFIG_TEMP_11_BITS(reg);	
+	
+	if (HDC.config.humid_res == HDC_RES_14_BITS)
+		HDC_CONFIG_HUMID_14_BITS(reg);		
+	else if (HDC.config.humid_res == HDC_RES_11_BITS)
+		HDC_CONFIG_HUMID_11_BITS(reg);	
+	else if (HDC.config.humid_res == HDC_RES_8_BITS)
+		HDC_CONFIG_HUMID_8_BITS(reg);	
+	
+	/* Write the CONFIG register */
+	if (HAL_I2C_Mem_Write(&hi2c2, HDC_ADD, HDC_CONFIG_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, HDC_REG_SIZE, 10) == HAL_OK)
+		return H12R0_OK;
+	else
+		return H12R0_ERR_HDC1080_WRITE;
+}
 
+/*-----------------------------------------------------------*/
+
+/* --- Read HDC1080DMBT humidity and temperature sensors. 
+*/
+Module_Status HDC_Read(uint8_t sensor)
+{
+	Module_Status result = H12R0_OK;
+	uint16_t reg[2] = {0};
+	
+	/* Trigger measurements */
+	if (HDC.config.mode == HDC_MODE_TEMP_AND_HUMID && sensor == HDC_BOTH)
+	{
+		if (HAL_I2C_Mem_Write(&hi2c2, HDC_ADD, HDC_TEMP_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, 2*HDC_REG_SIZE, 10) != HAL_OK)
+			return H12R0_ERR_HDC1080_WRITE;		
+	}
+	else if (HDC.config.mode == HDC_MODE_TEMP_OR_HUMID)
+	{
+		if (sensor == HDC_TEMPERATURE)
+		{
+			if (HAL_I2C_Mem_Write(&hi2c2, HDC_ADD, HDC_TEMP_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, HDC_REG_SIZE, 10) != HAL_OK)
+				return H12R0_ERR_HDC1080_WRITE;		
+		}
+		else if (sensor == HDC_HUMIDITY)
+		{
+			if (HAL_I2C_Mem_Write(&hi2c2, HDC_ADD, HDC_HUMID_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, HDC_REG_SIZE, 10) != HAL_OK)
+				return H12R0_ERR_HDC1080_WRITE;		
+		}
+	}
+	
+	/* Wait until measurements are ready */
+	Delay_ms(10);
+	
+	/* Read sensor data */
+	if (HDC.config.mode == HDC_MODE_TEMP_AND_HUMID && sensor == HDC_BOTH)
+	{
+		if (HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_TEMP_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, 2*HDC_REG_SIZE, 10) != HAL_OK)
+			return H12R0_ERR_HDC1080_READ;		
+		
+		HDC.temperature = ((float)reg[0]/65536.0f) * 165.0f - 40.0f;
+		HDC.humidity = ((float)reg[1]/65536.0f) * 100.0f;
+	}
+	else if (HDC.config.mode == HDC_MODE_TEMP_OR_HUMID)
+	{
+		if (sensor == HDC_TEMPERATURE)
+		{
+			if (HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_TEMP_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, HDC_REG_SIZE, 10) != HAL_OK)
+				return H12R0_ERR_HDC1080_READ;	
+
+			HDC.temperature = ((float)reg[0]/65536.0f) * 165.0f - 40.0f;			
+		}
+		else if (sensor == HDC_HUMIDITY)
+		{
+			if (HAL_I2C_Mem_Read(&hi2c2, HDC_ADD, HDC_HUMID_REG_ADD, HDC_REG_ADD_SIZE, (uint8_t *)&reg, HDC_REG_SIZE, 10) != HAL_OK)
+				return H12R0_ERR_HDC1080_READ;
+
+			HDC.humidity = ((float)reg[1]/65536.0f) * 100.0f;
+		}
+	}
+	
+	return result;
+}
 
 
 /* -----------------------------------------------------------------------
--- H12R0/H12R0.h
+++ H12R0/H12R0.h
@@ -93,10 +93,37 @@
 
 /* Module-specific Definitions */
 #ifdef H12R0
-	#define _I2C2_GPIO_PORT						GPIOA
-	#define _I2C2_SDA_PIN							GPIO_PIN_12
-	#define _I2C2_SCL_PIN							GPIO_PIN_11
-	#define _I2C2_GPIO_AF							GPIO_AF5_I2C2
+	#define _I2C2_GPIO_PORT				GPIOA
+	#define _I2C2_SDA_PIN					GPIO_PIN_12
+	#define _I2C2_SCL_PIN					GPIO_PIN_11
+	#define _I2C2_GPIO_AF					GPIO_AF5_I2C2
+	
+	/* HDC1080 section */
+	#define HDC_ADD												0x40		/* 7-bit address */
+	#define HDC_TEMP_REG_ADD							0x00
+	#define HDC_HUMID_REG_ADD							0x01
+	#define HDC_CONFIG_REG_ADD						0x02
+	#define HDC_SERIAL_ID_3								0xFB
+	#define HDC_SERIAL_ID_2								0xFC
+	#define HDC_SERIAL_ID_1								0xFD
+	#define HDC_MANUFACTURER_ID						0xFE
+	#define HDC_DEVICE_ID									0xFF
+	#define HDC_REG_SIZE									2
+	#define HDC_REG_ADD_SIZE							1
+	#define HDC_CONFIG_RESET(regval)						(regval |= 0x8000)		
+	#define HDC_CONFIG_HEAT_ENABLE(regval)			(regval |= 0x2000)
+	#define HDC_CONFIG_HEAT_DISABLE(regval)			(regval &= 0xDFFF)
+	#define HDC_CONFIG_TEMP_AND_HUMID(regval)		(regval |= 0x1000)
+	#define HDC_CONFIG_TEMP_OR_HUMID(regval)		(regval &= 0xEFFF)
+	#define HDC_CONFIG_BAT_BELOW_2_8(regval)		(regval >> 11)		
+	#define HDC_CONFIG_TEMP_14_BITS(regval)			(regval &= 0xFBFF)
+	#define HDC_CONFIG_TEMP_11_BITS(regval)			(regval |= 0x0400)
+	#define HDC_CONFIG_HUMID_14_BITS(regval)		(regval &= 0xFCFF)
+	#define HDC_CONFIG_HUMID_11_BITS(regval)		(regval &= 0xFDFF)
+	#define HDC_CONFIG_HUMID_8_BITS(regval)			(regval &= 0xFEFF)
+	enum HDC_e{HDC_RESET, HDC_HEAT_ENABLE, HDC_HEAT_DISABLE, HDC_MODE_TEMP_AND_HUMID, HDC_MODE_TEMP_OR_HUMID,
+									HDC_RES_14_BITS, HDC_RES_11_BITS, HDC_RES_8_BITS, HDC_TEMPERATURE, HDC_HUMIDITY, HDC_BOTH};
+	
 #endif
 
 /* Module_Status Type Definition */  
@@ -104,14 +131,36 @@ typedef enum
 {
   H12R0_OK = 0,
 	H12R0_ERR_UnknownMessage,
-  H12R0_ERR_WrongColor,
-	H12R0_ERR_WrongIntensity,
+  H12R0_ERR_HDC1080_MID,
+	H12R0_ERR_HDC1080_CONFIG,
+	H12R0_ERR_HDC1080_READ,
+	H12R0_ERR_HDC1080_WRITE,
 	H12R0_ERROR = 255
 } Module_Status;
 
 /* Indicator LED */
 #define _IND_LED_PORT		GPIOA
 #define _IND_LED_PIN		GPIO_PIN_7
+
+
+/* HDC1080 configuration struct */
+typedef struct
+{
+	uint8_t reset;				// Software Reset: HDC_RESET
+	uint8_t heat;					// Heater element: HDC_HEAT_ENABLE, HDC_HEAT_DISABLE
+	uint8_t mode;					// Mode of acquisition: HDC_MODE_TEMP_AND_HUMID, HDC_MODE_TEMP_OR_HUMID
+	uint8_t temp_res;			// Temperature Measurement Resolution: HDC_RES_14_BITS, HDC_RES_11_BITS
+	uint8_t humid_res;		// Humidity Measurement Resolution: HDC_RES_14_BITS, HDC_RES_11_BITS, HDC_RES_8_BITS
+} HDC_config_t;
+
+/* HDC1080 struct */
+typedef struct
+{
+	HDC_config_t config;
+	float temperature;
+	float humidity;
+} HDC_t;
+extern HDC_t HDC;
 
 
 /* Export UART variables */
@@ -146,6 +195,12 @@ extern void MX_USART6_UART_Init(void);
    ----------------------------------------------------------------------- 
 */
 
+#define HDC_Sample_Temperature()		HDC_Read(HDC_TEMPERATURE)
+#define HDC_Sample_Humidity()				HDC_Read(HDC_HUMIDITY)
+#define HDC_Sample_Sensors()				HDC_Read(HDC_BOTH)
+
+extern Module_Status HDC_Config(void);
+extern Module_Status HDC_Read(uint8_t sensor);
 
 /* -----------------------------------------------------------------------
 	|															Commands																 	|
