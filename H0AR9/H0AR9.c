9476dce38848139ec49ba0aeaf4792f1cfa0396d
Asaad K <masaadkaadan@gmail.com>
2019-08-24 21:47:40 +0300

    UART ISR module driver fix DMA module driver fix Adding shift variable to module msg parser submodule update

--- H0AR9/H0AR9.c
+++ H0AR9/H0AR9.c
@@ -90,7 +90,7 @@ void Module_Init(void)
 
 /* --- H0AR9 message processing task. 
 */
-Module_Status Module_MessagingTask(uint16_t code, uint8_t port, uint8_t src, uint8_t dst)
+Module_Status Module_MessagingTask(uint16_t code, uint8_t port, uint8_t src, uint8_t dst, uint8_t shift)
 {
 	Module_Status result = H0AR9_OK;
 	
