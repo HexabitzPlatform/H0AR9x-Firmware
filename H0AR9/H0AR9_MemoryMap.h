cbe5e2bd89a74d05f77ce0df2a4446237f68bae5
DESKTOP-BI5TT6Q\User <israaalkadour1998@gmail.com>
2022-10-06 11:29:58 +0300

    Edit Memory size & update

--- H0AR9/H0AR9_MemoryMap.h
+++ H0AR9/H0AR9_MemoryMap.h
@@ -15,26 +15,27 @@
  extern "C" {
 #endif
 
-/* Memory map: - STM32F091CB
- - Application: 0x08000000 - 0x0801D7FF >> 118 KB
- - Read-only (RO): 0x0801D800 - 0x0801DFFF >> 2 KB, used to store topology information and Command Snippets
- - Emulated EEPROM: 0x0801E000 - 0x0801FFFF >> 8 KB, fits 1024 16-bit variables in 2 main-duplicate pages (A and B)
- */
-#ifdef FLASH_SIZE
-#undef FLASH_SIZE
-#endif
-#define APP_START_ADDRESS  		((uint32_t)0x08000000) 
-#define RO_START_ADDRESS  		((uint32_t)0x08073000)
-#define RO_MID_ADDRESS  		((uint32_t)0x0807D8EF) 		// Snippets are stored here
-#define EEPROM_START_ADDRESS  	((uint32_t)0x0807E0BF)
-#define FLASH_SIZE				((uint32_t)0x800000UL)			// All sizes in bytes
-#define SRAM_SIZE				((uint32_t)0x8000)
-#define PAGE_SIZE               ((uint32_t)0x0800)  		/* Page size = 2KByte for STM32F07x and STM32F09x devices */
-#define NumOfPages				(FLASH_SIZE/PAGE_SIZE)
+ /* Memory map: - STM32G0B1CE
+  - Application: 0x08000000 - 0x0807D7FF >> 502 KB
+  - Read-only (RO): 0x0807D800 - 0x0807DFFF >> 2 KB, used to store topology information and Command Snippets
+  - Emulated EEPROM: 0x0807E000 - 0x0807FFFF >>8 KB, fits 1024 16-bit variables in 2 main-duplicate pages (A and B)
+  */
+ #ifdef FLASH_SIZE
+	#undef FLASH_SIZE
+ #endif
 
-#ifdef __cplusplus
-}
-#endif
+ #define APP_START_ADDRESS  		((uint32_t)0x08000000)
+ #define RO_START_ADDRESS  		((uint32_t)0x0807D800)
+ #define RO_MID_ADDRESS  		((uint32_t)0x0807D400) 		// Snippets are stored here
+ #define EEPROM_START_ADDRESS  	((uint32_t)0x0807E000)
+ #define FLASH_SIZE				((uint32_t)0x80000)	        //TOBECHECKED		// All sizes in bytes
+ #define SRAM_SIZE				((uint32_t)0x24000)         //size for G0=144kb
+ #define PAGE_SIZE               ((uint32_t)0x0800)  		/* Page size = 2KByte  */
+ #define NumOfPages				(FLASH_SIZE/PAGE_SIZE)
+
+ #ifdef __cplusplus
+ }
+ #endif
 
 #endif /* H0AR9_MEMORYMAP_H */
 
